874e784cb2bcfc8e4845832a8bd88520b3453992
cho eun jae <jej059813@naver.com>
2025-05-15 20:49:25 +0900

    융합관 강의실 좌석 사전 선택 프로그램

--- javaproject/src/Main.java
+++ javaproject/src/Main.java
@@ -2,12 +2,24 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.printf("Hello and welcome!");
+        // 프로그램 실행
+        System.out.println("=== 융합관 강의실 좌석 사전 선택 프로그램 ===");
 
-        for (int i = 1; i <= 5; i++) {
+        // 이름 입력
+        System.out.println("이름을 입력하세요: ");
+        String name = scanner.nextLine();   // 사용자로부터 문자열(이름) 입력 받기
 
-            System.out.println("i = " + i);
-        }
+        // 학번 입력
+        System.out.println("학번을 입력하세요: ");
+        String studentId = scanner.nextLine();  // 사용자로부터 문자열(학번) 입력 받기
+
+        // 입력한 값 출력
+        System.out.println("\n=== 입력한 정보를 확인하세요. ===");
+        System.out.println("이름: " + name);
+        System.out.println("학번: "+ studentId);
+
+        scanner.close();    // 스캐너 종료 (메모리 낭비 방지)
     }
 }
